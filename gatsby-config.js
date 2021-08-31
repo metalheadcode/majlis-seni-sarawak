882bd117781a7f880d7e361ea9d910308eb5d02c
metalheadcode <metalheadcoder@gmail.com>
2021-08-31 23:01:31 +0800

    change siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Majlis Seni Sarawak`,
+    description: `Laman sesawang bagi pengiat seni di Sarawak (MSS)`,
+    author: `@metalheadcoder`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
