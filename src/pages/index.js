1c3e3504e64231dec2ae56c188867c094fde51c7
metalheadcode <metalheadcoder@gmail.com>
2021-09-02 23:46:23 +0800

    applying section two

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import HeroLayoutOne from "../components/Heroes/HeroLayoutOne"
+import HeroLayoutOne from "../components/Sections/HeroLayoutOne"
+import SectionTwo from "../components/Sections/SectionTwo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <HeroLayoutOne />
+    <SectionTwo />
   </Layout>
 )
 
