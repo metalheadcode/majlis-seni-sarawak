dbeacbfdb1488d3c6ecd80625a0dd13312274574
metalheadcode <metalheadcoder@gmail.com>
2021-09-03 21:41:23 +0800

    create HomeScreen

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import HeroLayoutOne from "../components/Sections/HeroLayoutOne"
+import HeroLayoutTwo from "../components/Sections/HeroLayoutTwo"
+import SectionTwo from "../components/Sections/SectionTwo"
+
+const Homescreen = () => {
+  const [scrollY, setScrollY] = useState()
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  const handleScroll = () => setScrollY(window.pageYOffset)
+  return (
+    <div>
+      <WrapperGlobal>
+        <HeroLayoutOne scrollY={scrollY} />
+        <HeroLayoutTwo scrollY={scrollY} />
+        <SectionTwo />
+      </WrapperGlobal>
+    </div>
+  )
+}
+
+export default Homescreen
+
+const WrapperGlobal = styled.div`
+  width: 100vw;
+  overflow: hidden;
+`
