bcffa587bdafcb9fddbf55570052efc4967cfd19
metalheadcode <metalheadcoder@gmail.com>
2021-09-05 21:54:29 +0800

    tryin fix the logic for menu display, but still wont work

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 import styled from "styled-components";
+import { Link } from "gatsby";
 
-const Menu = () => {
+const Menu = ({ state }) => {
+  const menuWrapper = useRef(null);
+
+  useEffect(() => {
+    if (state.clicked === false) {
+      gsap.to(menuWrapper.current, {
+        height: 0,
+        duration: 1,
+        ease: "Power3.easeIn",
+      });
+      gsap.to(menuWrapper.current, {
+        duration: 1,
+        css: {
+          display: "none",
+        },
+      });
+    } else if (state.initial === null && state.clicked === true) {
+      gsap.to(menuWrapper.current, {
+        duration: 0,
+        css: {
+          display: "grid",
+        },
+      });
+      console.log("Animation 1");
+      gsap.from(menuWrapper.current, {
+        height: 0,
+        duration: 1,
+        ease: "Power3.easeOut",
+      });
+      console.log("Animation 2");
+    }
+  }, [state]);
+
   return (
-    <MenuWrapper>
+    <MenuWrapper ref={menuWrapper}>
       <MenuItems>
-        <h1>Sarawak Artist</h1>
+        <Link to={"/artist"}>Sarawak Artist</Link>
       </MenuItems>
       <MenuItems>
-        <h1>About MSS</h1>
+        <Link to={"/about"}>About MSS</Link>
       </MenuItems>
       <MenuItems>
-        <h1>Contact</h1>
+        <Link to={"/contact"}>Contact</Link>
       </MenuItems>
       <MenuItems>
-        <h1>Grant</h1>
+        <Link to={"/grant"}>Grant</Link>
       </MenuItems>
     </MenuWrapper>
   );
@@ -23,27 +57,31 @@ const Menu = () => {
 export default Menu;
 
 const MenuWrapper = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
   width: 100vw;
   background-color: #d66a50;
   z-index: 100;
-  display: grid;
-  gap: 20px;
+  display: none;
 `;
 
 const MenuItems = styled.div`
-  padding: 20px;
+  padding: 30px;
+  text-align: center;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 
-  h1 {
+  a {
     font-size: 60px;
     color: white;
-    text-align: center;
   }
 
   &:hover {
+    cursor: pointer;
     background-color: #ffbb54;
     transition: 1s cubic-bezier(0.165, 0.84, 0.44, 1);
 
-    h1 {
+    a {
       color: black;
     }
   }
