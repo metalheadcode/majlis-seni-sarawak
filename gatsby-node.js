1b56b62531d2e1bd6a66ef6df5e8bb78aa91b47d
metalheadcode <metalheadcoder@gmail.com>
2021-09-07 23:33:53 +0800

    setup gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,22 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { data } = await graphql(`
+    query PostQuery {
+      allDatoCmsPost {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `);
+
+  data.allDatoCmsPost.nodes.forEach(node => {
+    actions.createPage({
+      path: "/blogs/" + node.slug,
+      component: path.resolve("./src/templates/BlogTemplate.js"),
+      context: { id: node.id },
+    });
+  });
+};
