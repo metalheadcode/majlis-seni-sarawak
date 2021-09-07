9f0f3a2ff54e7284fdb532a8815a6ade3831f44b
metalheadcode <metalheadcoder@gmail.com>
2021-09-07 23:34:16 +0800

    create card blog"

---
+++ src/components/Cards/CardBlogs.js
@@ -0,0 +1,52 @@
+import Img from "gatsby-image";
+import React from "react";
+import cn from "classnames";
+import styled from "styled-components";
+
+const CardBlogs = ({ author, coverImage, title, excerpt, slug, date }) => {
+  const usDate = new Date(date).toLocaleDateString("en-US");
+  console.log("Image resource", coverImage.small.src);
+  return (
+    <BlogCard>
+      <Img
+        fluid={{
+          ...coverImage.small,
+          alt: `Cover Image for ${title}`,
+        }}
+        className={cn("shadow-small", {
+          "hover:shadow-medium transition-shadow duration-200": slug,
+        })}
+      />
+      <Title>{title}</Title>
+      <AuthorAndDate>
+        <p>{author.name}</p>
+        <p>{usDate}</p>
+      </AuthorAndDate>
+      <p>{excerpt}</p>
+    </BlogCard>
+  );
+};
+
+export default CardBlogs;
+
+const BlogCard = styled.div`
+  padding: 20px;
+  display: grid;
+  gap: 15px;
+  cursor: pointer;
+  background-color: white;
+  border: 3px solid black;
+  box-shadow: 7px 10px 0px 2px #d66a50;
+  -webkit-box-shadow: 7px 10px 0px 2px #d66a50;
+  -moz-box-shadow: 7px 10px 0px 2px #d66a50;
+`;
+const Title = styled.div`
+  font-size: 24px;
+`;
+
+const AuthorAndDate = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: rgba(0, 0, 0, 0.5);
+  font-family: sans-serif;
+`;
