e342efe92005433eddf7815422cfcc665ecf6ab5
metalheadcode <metalheadcoder@gmail.com>
2021-09-07 23:34:43 +0800

    create homescreen

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { BlogPosts } from "../components/Sections/BlogPosts";
 import HeroLayoutOne from "../components/Sections/HeroLayoutOne";
 import HeroLayoutTwo from "../components/Sections/HeroLayoutTwo";
 import SectionTwo from "../components/Sections/SectionTwo";
@@ -12,13 +13,17 @@ const Homescreen = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [scrollY]);
 
-  const handleScroll = () => setScrollY(window.pageYOffset);
+  const handleScroll = () => {
+    setScrollY(window.pageYOffset);
+  };
+
   return (
     <div>
       <WrapperGlobal>
         <HeroLayoutOne scrollY={scrollY} />
+        <BlogPosts />
         <SectionTwo scrollY={scrollY} />
         <HeroLayoutTwo scrollY={scrollY} />
       </WrapperGlobal>
