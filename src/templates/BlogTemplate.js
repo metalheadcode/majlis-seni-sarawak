33e21fb4016752c89bf8aa246c96588e8b0e9278
metalheadcode <metalheadcoder@gmail.com>
2021-09-09 23:55:16 +0800

    edit blog template

--- src/templates/BlogTemplate.js
+++ src/templates/BlogTemplate.js
@@ -1,58 +1,64 @@
 import React from "react";
-import { graphql } from "gatsby";
-import cn from classnames;
-import Image from "gatsby-image";
+import Img from "gatsby-image";
 import { StructuredText } from "react-datocms";
 import styled from "styled-components";
 
 const BlogTemplate = ({ data }) => {
   const { title } = data.datoCmsPost;
   const { content } = data.datoCmsPost;
-  console.log(content);
+  const { coverImage } = data.datoCmsPost;
+  console.log("CoverImage", coverImage);
   return (
     <BlogWrapper>
+      <Img fluid={coverImage.fluid} />
       <h1>{title}</h1>
-      <StructuredText
-        data={content}
-        renderBlock={({ record }) => {
-          console.log("Record", record);
-          <Image
-            fluid={{
-              ...coverImage.small,
-              alt: `${record}`,
-            }}
-            className={cn("shadow-small", {
-              "hover:shadow-medium transition-shadow duration-200": slug,
-            })}
-          />;
-        }}
-      />
+      <div style={{ padding: "20px" }}>
+        <StructuredText
+          data={content}
+          renderBlock={({ record }) => {
+            console.log("record", record);
+            if (record.__typename === "DatoCmsImageBlock") {
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <Img
+                  fluid={record.image.fluid}
+                  style={{
+                    display: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    padding: "20px",
+                  }}
+                />
+                ;
+              </div>;
+            }
+
+            return (
+              <>
+                <p>Don't know how to render a block!</p>
+                <pre>{JSON.stringify(record, null, 2)}</pre>
+              </>
+            );
+          }}
+        />
+      </div>
     </BlogWrapper>
   );
 };
 
 export default BlogTemplate;
 
-export const query = graphql`
-  query SinglePostQuery($id: String) {
-    datoCmsPost(id: { eq: $id }) {
-      content {
-        value
-        blocks {
-          id: originalId
-          image {
-            fluid(imgixParams: { fm: "jpg" }) {
-              ...GatsbyDatoCmsFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
 const BlogWrapper = styled.div`
   max-width: 1240px;
+  height: 100%;
   margin: auto;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
