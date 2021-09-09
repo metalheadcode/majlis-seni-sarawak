d52c3bfad137fa4dfad2dd62e4b76ba7f81fdf6a
metalheadcode <metalheadcoder@gmail.com>
2021-09-09 23:55:42 +0800

    edit hero layout one

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,25 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Homescreen from "../screens/Homescreen";
+import { graphql } from "gatsby";
 
-const IndexPage = () => (
+const IndexPage = ({ data: { batikOne } }) => (
   <Layout>
     <Seo title="Home" />
-    <Homescreen />
+    <Homescreen batikOne={batikOne} />
   </Layout>
 );
 
 export default IndexPage;
+
+export const query = graphql`
+  {
+    batikOne: file(relativePath: { eq: "batik1.webp" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
