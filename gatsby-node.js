ada21b77ed1bce48ea5318a763a744d2370611a6
metalheadcode <metalheadcoder@gmail.com>
2021-09-09 23:53:15 +0800

    remove the conventional style of create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,59 @@
-const path = require("path");
+// const path = require("path");
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { data } = await graphql(`
-    query PostQuery {
-      allDatoCmsPost {
-        nodes {
-          id
-          slug
-        }
-      }
-    }
-  `);
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { data } = await graphql(`
+//     query PostQuery {
+//       allDatoCmsPost {
+//         nodes {
+//           id
+//           slug
+//         }
+//       }
+//       datoCmsPost(id: { eq: $id }) {
+//         content {
+//           value
+//           blocks {
+//             id: originalId
+//             __typename
+//             ... on DatoCmsImageBlock {
+//               image {
+//                 fluid {
+//                   ...GatsbyDatoCmsFluid
+//                 }
+//               }
+//             }
+//             children {
+//               ... on DatoCmsImageBlock {
+//                 id
+//                 image {
+//                   fluid {
+//                     ...GatsbyDatoCmsFluid
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//         coverImage {
+//           fluid {
+//             ...GatsbyDatoCmsFluid
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  data.allDatoCmsPost.nodes.forEach(node => {
-    actions.createPage({
-      path: "/blogs/" + node.slug,
-      component: path.resolve("./src/templates/BlogTemplate.js"),
-      context: { id: node.id },
-    });
-  });
-};
+//   data.allDatoCmsPost.nodes.forEach(node => {
+//     actions.createPage({
+//       path: "/blogs/" + node.slug,
+//       component: path.resolve("./src/templates/BlogTemplate.js"),
+//       context: { id: node.id },
+//     });
+//   });
+// };
+
+// export const query = graphql`
+//   query SinglePostQuery($id: String) {
+
+//   }
+// `;
