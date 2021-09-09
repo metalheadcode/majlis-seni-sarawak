9e73729e9394a2d6a0b501154420b639b47ed132
metalheadcode <metalheadcoder@gmail.com>
2021-09-09 23:54:37 +0800

    create more posts component

---
+++ src/components/Posts/MorePost.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MorePost = ({ morePost }) => {
+  console.log(morePost);
+  return (
+    <div>
+      <h1>This is more posts</h1>
+    </div>
+  );
+};
+
+export default MorePost;
