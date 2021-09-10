ec70bec3d441642f66593fa592e7dbb078884a09
metalheadcode <metalheadcoder@gmail.com>
2021-09-10 22:00:44 +0800

    create morePost component

--- src/components/Posts/MorePost.js
+++ src/components/Posts/MorePost.js
@@ -1,12 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+import CardBlogs from "../Cards/CardBlogs";
 
-const MorePost = ({ morePost }) => {
-  console.log(morePost);
+const MorePost = ({ title, morePosts }) => {
   return (
-    <div>
-      <h1>This is more posts</h1>
-    </div>
+    <CardsWrapper>
+      <h1>{title}</h1>
+      <GridWrapper>
+        {morePosts.nodes.length > 1 &&
+          morePosts.nodes.map((blog, index) => (
+            <CardBlogs
+              key={index}
+              author={blog.author}
+              coverImage={blog.coverImage}
+              title={blog.title}
+              excerpt={blog.excerpt}
+              slug={blog.slug}
+              date={blog.date}
+            />
+          ))}
+      </GridWrapper>
+    </CardsWrapper>
   );
 };
 
 export default MorePost;
+
+const CardsWrapper = styled.div`
+  max-width: 1240px;
+  margin: auto;
+  padding: 100px 20px;
+`;
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 425px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+// margin: 100px auto;
+// padding: 0 20px;
