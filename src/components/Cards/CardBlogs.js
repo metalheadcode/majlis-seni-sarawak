386a727a2ca8902ce36d2f4e361f12d92f6e1132
metalheadcode <metalheadcoder@gmail.com>
2021-09-10 21:58:23 +0800

    imporve card blog responsive

--- src/components/Cards/CardBlogs.js
+++ src/components/Cards/CardBlogs.js
@@ -2,34 +2,38 @@ import Img from "gatsby-image";
 import React from "react";
 import cn from "classnames";
 import styled from "styled-components";
+import { Link } from "gatsby";
 
 const CardBlogs = ({ author, coverImage, title, excerpt, slug, date }) => {
   const usDate = new Date(date).toLocaleDateString("en-US");
-  console.log("Image resource", coverImage.small.src);
+
   return (
-    <BlogCard>
-      <Img
-        fluid={{
-          ...coverImage.small,
-          alt: `Cover Image for ${title}`,
-        }}
-        className={cn("shadow-small", {
-          "hover:shadow-medium transition-shadow duration-200": slug,
-        })}
-      />
-      <Title>{title}</Title>
-      <AuthorAndDate>
-        <p>{author.name}</p>
-        <p>{usDate}</p>
-      </AuthorAndDate>
-      <p>{excerpt}</p>
-    </BlogCard>
+    <Link to={`/posts/${slug}`} style={{ textDecoration: "none" }}>
+      <BlogCard>
+        <Img
+          fluid={{
+            ...coverImage.small,
+            alt: `Cover Image for ${title}`,
+          }}
+          className={cn("shadow-small", {
+            "hover:shadow-medium transition-shadow duration-200": slug,
+          })}
+        />
+        <Title>{title}</Title>
+        <AuthorAndDate>
+          <p>{author.name}</p>
+          <p>{usDate}</p>
+        </AuthorAndDate>
+        <p>{excerpt}</p>
+      </BlogCard>
+    </Link>
   );
 };
 
 export default CardBlogs;
 
 const BlogCard = styled.div`
+  color: black;
   padding: 20px;
   display: grid;
   gap: 15px;
@@ -39,9 +43,27 @@ const BlogCard = styled.div`
   box-shadow: 7px 10px 0px 2px #d66a50;
   -webkit-box-shadow: 7px 10px 0px 2px #d66a50;
   -moz-box-shadow: 7px 10px 0px 2px #d66a50;
+  transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    transform: translate(-2px, -2px);
+    box-shadow: 10px 12px 0px 2px #d66a50;
+    -webkit-box-shadow: 10px 12px 0px 2px #d66a50;
+    -moz-box-shadow: 10px 12px 0px 2px #d66a50;
+  }
+  &:active {
+    transform: translate(2px, 2px);
+    background-color: #ffbb54;
+  }
+
+  p {
+    margin: 0;
+  }
 `;
+
 const Title = styled.div`
   font-size: 24px;
+  font-weight: 700;
 `;
 
 const AuthorAndDate = styled.div`
