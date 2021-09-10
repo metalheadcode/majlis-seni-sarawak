66a4370f296cccdc07e4a4e5c79261e2507fa88d
metalheadcode <metalheadcoder@gmail.com>
2021-09-10 22:02:01 +0800

    move graphql query to page index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Seo from "../components/seo";
 import Homescreen from "../screens/Homescreen";
 import { graphql } from "gatsby";
 
-const IndexPage = ({ data: { batikOne } }) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <Homescreen batikOne={batikOne} />
+    <Homescreen data={data} />
   </Layout>
 );
 
@@ -17,8 +17,64 @@ export const query = graphql`
   {
     batikOne: file(relativePath: { eq: "batik1.webp" }) {
       childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
+        fixed {
+          src
+        }
+      }
+    }
+    batikTwo: file(relativePath: { eq: "batik2.webp" }) {
+      childImageSharp {
+        fixed {
+          src
+        }
+      }
+    }
+    birdOne: file(relativePath: { eq: "bird1.webp" }) {
+      childImageSharp {
+        fixed {
+          src
+        }
+      }
+    }
+    birdTwo: file(relativePath: { eq: "bird2.webp" }) {
+      childImageSharp {
+        fixed {
+          src
+        }
+      }
+    }
+    birdThree: file(relativePath: { eq: "bird3.webp" }) {
+      childImageSharp {
+        fixed {
+          src
+        }
+      }
+    }
+    morePosts: allDatoCmsPost(sort: { fields: date, order: DESC }, limit: 4) {
+      nodes {
+        title
+        slug
+        excerpt
+        date
+        coverImage {
+          small: fluid(
+            imgixParams: { fm: "jpg" }
+            sizes: "(max-width: 760px) 100vw, (max-width: 1500px) 50vw, 700px"
+          ) {
+            ...GatsbyDatoCmsFluid
+          }
+        }
+        author {
+          name
+          picture {
+            fixed(
+              width: 48
+              height: 48
+              imgixParams: { fm: "jpg", fit: "crop", sat: -100 }
+            ) {
+              ...GatsbyDatoCmsFixed
+            }
+          }
         }
       }
     }
